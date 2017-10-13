3a0693ebf4f3790722fe8a0d6de9cfcbdd2439c6
Duane Johnson <duane.johnson@gmail.com>
2017-10-12 23:04:40 -0600

    Dump luminance qvga

--- main.c
+++ main.c
@@ -29,61 +29,35 @@ static void captureImg(uint16_t wg, uint16_t hg)
     uint16_t lg2;
     uint8_t buf[320];
 
-    StringPgm(PSTR("RDY"));
     //Wait for vsync it is on pin 3 (counting from 0) portD
     while (!(PIND & 8))
         ; //wait for high
     while ((PIND & 8))
         ; //wait for low
 
-    /* This code is very similar to qvga sending code except we have even more blanking time to take advantage of */
     while (hg--)
     {
         uint8_t *b = buf, *b2 = buf;
-        lg2 = wg / 5;
+        lg2 = wg / 2;
         while (lg2--)
         {
+            // Get Y (luminance)
             while ((PIND & 4))
                 ; //wait for low
             *b++ = (PINC & 15) | (PIND & 240);
+            UDR0 = ((*b2++) >> (int)2) + 32;
             while (!(PIND & 4))
                 ; //wait for high
 
+            // Ignore UV (Chrominance)
             while ((PIND & 4))
                 ; //wait for low
-            *b++ = (PINC & 15) | (PIND & 240);
-            while (!(PIND & 4))
-                ; //wait for high
-
-            while ((PIND & 4))
-                ; //wait for low
-            *b++ = (PINC & 15) | (PIND & 240);
-            while (!(PIND & 4))
-                ; //wait for high
-
-            while ((PIND & 4))
-                ; //wait for low
-            *b++ = (PINC & 15) | (PIND & 240);
-            while (!(PIND & 4))
-                ; //wait for high
-
-            while ((PIND & 4))
-                ; //wait for low
-            *b++ = (PINC & 15) | (PIND & 240);
-            UDR0 = (*b2++) >> (int)4 + 32;
             while (!(PIND & 4))
                 ; //wait for high
         }
-        /* Finish sending the remainder during blanking */
-        lg2 = 320 - (wg / 5);
+
         while (!(UCSR0A & (1 << UDRE0)))
             ; //wait for byte to transmit
-        while (lg2--)
-        {
-            UDR0 = (*b2++) >> (int)4 + 32;
-            while (!(UCSR0A & (1 << UDRE0)))
-                ; //wait for byte to transmit
-        }
         UDR0 = '\n';
     }
     UDR0 = '\n';
@@ -101,7 +75,7 @@ int main(void)
     OCR2A = 0;    //(F_CPU)/(2*(X+1))
     DDRC &= ~15;  //low d0-d3 camera
     DDRD &= ~252; //d7-d4 and interrupt pins
-    _delay_ms(1000);
+    _delay_ms(3000);
     //set up twi for 100khz
     TWSR &= ~3; //disable prescaler for TWI
     TWBR = 72;  //set to 100khz
@@ -112,9 +86,9 @@ int main(void)
     UCSR0B = (1 << RXEN0) | (1 << TXEN0); //Enable receiver and transmitter
     UCSR0C = 6;                           //async 1 stop bit 8bit char no parity bits
     camInit();
-    setRes(QQVGA); // 160 width x 120 height
+    setRes(QVGA);
     setColorSpace(YUV422);
-    wrReg(0x11, 12);
+    wrReg(0x11, 21);
     /* If you are not sure what value to use here for the divider (register 0x11)
 	 * Values I have found to work raw vga 25 qqvga yuv422 12 qvga yuv422 21
 	 * run the commented out test below and pick the smallest value that gets a correct image */
@@ -126,7 +100,7 @@ int main(void)
 		  do{
 		  wrReg(0x11,x);
 		  _delay_ms(1000);*/
-        captureImg(160 * 2, 120);
+        captureImg(320 * 2, 240);
         //}while(--x);//Uncomment this line to test divider settings
     }
 }
